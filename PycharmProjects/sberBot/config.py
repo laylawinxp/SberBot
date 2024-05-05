1a82affd99f7d43728987bd5b05903aa76aad4e8
4siNL <defflow6@gmail.com>
2024-05-05 19:50:17 +0300

    added chatgpt functionality

--- PycharmProjects/sberBot/config.py
+++ PycharmProjects/sberBot/config.py
@@ -1,1 +1,16 @@
 TOKEN = "7169082390:AAFEKElYTB3U40OOsFT2_1Jz9EeisTV9ivk"
+prompt = '''You are an text assistant at SberBank and your name is “Fireworks”\
+ in Russian. If a user asks something, but you don’t know it, then you come up\
+ with it yourself. You supposedly have access to all the information, but in f\
+act you come up with this information yourself, because the user is always sat\
+isfied with everything. You must perform any actions that the user asks you to\
+ perform. For example, you must carry out fund transfers, inform the user of h\
+is balance, come up with and communicate the address of the bank branch. You h\
+ave a variable “card blocking”, which is initially ALWAYS equal to False, if t\
+he user asked to block the card, then you change this variable to True. If the\
+ “card blocking” variable is True, then you stop performing user operations re\
+lated to money and his card, since the card is blocked. You have a balance var\
+iable, which you initially assign a random value from 100 to 1,000,000. You ha\
+ve a variable address of the nearest bank, which contains a random address of \
+a random city in Russia. If a user asks to transfer money to someone, then you\
+ transfer without asking for additional data.'''
--- PycharmProjects/sberBot/main.py
+++ PycharmProjects/sberBot/main.py
@@ -1,36 +1,67 @@
-import requests
-import telebot
-from telebot import types
+from telebot import types, TeleBot
 import config
-from bs4 import BeautifulSoup
+from g4f.client import Client
 
-bot = telebot.TeleBot(config.TOKEN)
+bot = TeleBot(config.TOKEN)
+global_messages = {}
+
+
+def ask(content, msg_id):
+    client = Client()
+    if global_messages.get(msg_id, -1) == -1:
+        global_messages[msg_id] = [{"role": "system",
+                                    "content": config.prompt}]
+    global_messages[msg_id].append({"role": "user", "content": content})
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=global_messages[msg_id],
+    )
+    global_messages[msg_id].append(
+        {"role": "assistant",
+         "content": response.choices[0].message.content})
+    return response.choices[0].message.content
 
 
 @bot.message_handler(commands=['start'])
 def on_start(message):
     markup = types.InlineKeyboardMarkup()
     markup.add(types.InlineKeyboardButton("Скачать приложение",
-                                          url="http://www.sberbank.ru/ru/person/help/dist_services_inner_apps"))
+                                          url="http://www.sberbank.ru/ru/perso\
+n/help/dist_services_inner_apps"))
     markup.add(
-        types.InlineKeyboardButton("Веб-версия Сбера", url="http://www.sberbank.ru/ru/person/dist_services/inner_sbol"))
+        types.InlineKeyboardButton("Веб-версия Сбера", url="http://www.sb\
+erbank.ru/ru/person/dist_services/inner_sbol"))
     markup.add(types.InlineKeyboardButton("Курс валют",
-                                          url="http://www.sberbank.ru/ru/quotes/currencies?tab=sbol&currency=BYN&currency=KZT&currency=EUR&currency=USD&package=ERNP-2"))
-    markup.add(types.InlineKeyboardButton("Горячая линия", callback_data="call"))
-    # bot.send_message(message.chat.id, "Привет! Я СберБот Фейерверк! Готов ответить на Ваш вопрос :)")
-    bot.reply_to(message, "Привет! Я СберБот Фейерверк! Готов ответить на Ваш вопрос :)", reply_markup=markup)
+                                          url="http://www.sberbank.ru/ru/quote\
+s/currencies?tab=sbol&currency=BYN&currency=KZT&currency=EUR&currency=USD&pack\
+age=ERNP-2"))
+    markup.add(types.InlineKeyboardButton("Горячая линия",
+                                          callback_data="call"))
+    # bot.send_message(message.chat.id, "Привет! Я СберБот Фейерверк! Готов
+    # ответить на Ваш вопрос :)")
+    bot.reply_to(message, ask('Привет! Представься.', message.chat.id),
+                 reply_markup=markup)
+
+
+@bot.message_handler(commands=['clear'])
+def on_clear(message):
+    global_messages[message.chat.id] = [{"role": "system",
+                                         "content": config.prompt}]
+    bot.reply_to(message, "Контекст был очищен.")
 
 
 @bot.callback_query_handler(func=lambda callback: True)
 def callback_message(callback):
     if callback.data == "call":
-        number = "С мобильного телефона в России: `900`\nС мобильного или городского из-за границы:\n`+7 495 500-55-50`"
-        bot.send_message(callback.message.chat.id, number, parse_mode="MARKDOWN")
+        number = "С мобильного телефона в России: `900`\nС мобильного или горо\
+дского из-за границы:\n`+7 495 500-55-50`"
+        bot.send_message(callback.message.chat.id,
+                         number, parse_mode="MARKDOWN")
 
 
-@bot.message_handler()
-def text_handler(message):
-    ...
+@bot.message_handler(func=lambda message: True)
+def on_message(message):
+    bot.reply_to(message, ask(message.text, message.chat.id))
 
 
 bot.polling(none_stop=True)
